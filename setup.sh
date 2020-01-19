c59f7fade3ead5606e4f94f3974dc7e2d674e6a3
Art Yang <artyang.ca@gmail.com>
2020-01-18 20:31:41 -0800

    Added system setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+apt-get update
+sudo apt-get install python3.7
+
+#zsh plugins
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.zsh/powerlevel9k
+git clone https://github.com/matthieusb/zsh-sdkman.git ~/.zsh/zsh-sdkman
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+# jupyter install with ipython-sql support
+pip3 install --upgrade pip
+pip3 install jupyter
