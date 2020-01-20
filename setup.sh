20accbd91b1462b2f1ba40d4e2d3576f0306efee
art <art@art-Ubuntu18.04>
2020-01-20 10:05:48 -0800

    Tested and updated setup

--- setup.sh
+++ setup.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
-apt-get update
-sudo apt-get install python3.7
+sudo apt update
+sudo apt install git
+sudo apt install build-essential
+sudo apt install zsh
+chsh -s $(which zsh)
+sudo apt-get install neovim
+sudo apt-get install python3-neovim
+
 
 #zsh plugins
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.zsh/powerlevel9k
-git clone https://github.com/matthieusb/zsh-sdkman.git ~/.zsh/zsh-sdkman
+git clone https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+# .zshrc should be added first
+source ~/.zshrc
 
 # jupyter install with ipython-sql support
-pip3 install --upgrade pip
-pip3 install jupyter
-pip3 install ipython-sql
+sudo apt install python3-pip
+pip3 install --user notebook
+pip3 install --user ipython-sql
