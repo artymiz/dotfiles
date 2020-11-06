fd5246a4c735803fae39f0ff3df9ffe83c0e4db6
arty <arty@sfu.ca>
2020-11-05 23:15:37 -0800

    install lsd

--- setup.sh
+++ setup.sh
@@ -12,7 +12,14 @@ sudo apt-get install python3-neovim
 git clone https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-# .zshrc should be added first
+
+# install lsd
+wget https://github.com/Peltoche/lsd/releases/download/0.18.0/lsd_0.18.0_amd64.deb
+sudo dpkg -i lsd_0.18.0_amd64.deb
+rm lsd_0.18.0_amd64.deb
+
+# copy zshrc
+cp ./.zshrc ~
 source ~/.zshrc
 
 # jupyter install with ipython-sql support
@@ -20,4 +27,3 @@ sudo apt install python3-pip
 pip3 install --user notebook
 pip3 install --user ipython-sql
 
-cp ./.zshrc ~
