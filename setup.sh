371bd1559db1d4247958a2a1a13dd33a3e8d3e0b
arty <arty@sfu.ca>
2020-11-11 18:08:02 -0800

    boost install added

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+cwd=$(pwd)
 sudo apt update
-sudo apt install git
-sudo apt install build-essential cmake
+sudo apt install build-essential cmake g++ autotools-dev libicu-dev libbz2-dev libboost-all-dev
 sudo apt-get install libncurses5-dev libncursesw5-dev
-sudo apt-get install build-essential curl file git
+sudo apt-get install curl file git
 
 # install homebrew
 if ! command -v brew &> /dev/null
@@ -23,7 +23,24 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 mkdir ~/.config/nvim
 cp ./nvim/init.vim ~/.config/nvim
 
+# llvm binary
+cd ~
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
+tar -xvf clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
+mv clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04 clang+llvm-11.0.0
+rm clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
+cd cwd
 
+#boost library
+cd ~
+wget https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.bz2
+tar -xvjf boost_1_74_0.tar.bz2
+cd boost_1_74_0
+sudo ./bootstrap.sh --prefix=/usr --with-python=python3 && sudo ./b2 stage -j<N> threading=multi link=shared
+sudo ./b2 install threading=multi link=shared
+cd cwd
+
+#zsh
 sudo apt install zsh
 chsh -s $(which zsh)
 
