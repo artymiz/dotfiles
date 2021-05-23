2e36c4fca912bdeced89fe5a69be8fbec593da09
Art Yang <43259518+artymiz@users.noreply.github.com>
2021-05-23 13:03:51 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,10 +20,10 @@ then
   cd neovim
   make CMAKE_BUILD_TYPE=Release
   sudo make install
-  rm -r build/  # clear the CMake cache
-  make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
-  make install
-  export PATH="$HOME/neovim/bin:$PATH"
+#   rm -r build/  # clear the CMake cache
+#   make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+#   make install
+#   export PATH="$HOME/neovim/bin:$PATH"
   cd $cwd;
 else
   sudo snap install --edge nvim --classic
